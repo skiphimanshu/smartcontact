f5da72bd03181e82745d401649dfdd64b5dcead7
himanshukumar1107 <=>
2022-10-10 19:12:18 +0530

    Second Commit

--- src/main/java/com/smart/SmartcontactmanagerApplication.java
+++ src/main/java/com/smart/SmartcontactmanagerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartcontactmanagerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SmartcontactmanagerApplication.class, args);
+		// run here
 	}
 }
